515c1aa0aca912beaedae42966d750d50a8e1f39
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-05 00:09:18 +0300

    add more models

--- galleria/models.py
+++ galleria/models.py
@@ -6,5 +6,27 @@ class Image(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete = models.CASCADE)
+    author = models.ForeignKey(User, on_delete= models.CASCADE)
+    category = models.ForeignKey("Categories", on_delete= models.CASCADE)
+    location = models.ForeignKey("Location", on_delete= models.CASCADE)
+    
 
+    class Meta:
+        ordering = ['-pk']
+
+
+    def __str__(self):
+        return f'{self.name}'
+
+class Categories(models.Model):
+    category = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+class Location(models.Model):
+    location = models.CharField(max_length=40)
+
+    def __str__(self):
+        return self.location
