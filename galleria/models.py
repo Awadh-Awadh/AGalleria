6409785e175a8d96c84d8e950741567e06fb5af5
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-04 23:00:40 +0300

    add image model

--- galleria/models.py
+++ galleria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Image(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+
