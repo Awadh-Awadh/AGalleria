5f427c7b6bc67c06c9dffce22199af4fd8a6a30f
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-05 09:12:14 +0300

    add a new model

--- galleria/models.py
+++ galleria/models.py
@@ -3,30 +3,10 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Image(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=30)
     description = models.TextField()
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete= models.CASCADE)
-    category = models.ForeignKey("Categories", on_delete= models.CASCADE)
-    location = models.ForeignKey("Location", on_delete= models.CASCADE)
-    
-
-    class Meta:
-        ordering = ['-pk']
-
-
-    def __str__(self):
-        return f'{self.name}'
-
-class Categories(models.Model):
-    category = models.CharField(max_length=30)
+    date_posted = models.DateTimeField(default= timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
-
-
-class Location(models.Model):
-    location = models.CharField(max_length=40)
-
-    def __str__(self):
-        return self.location
