49f3e2a5b2c714d8c54a4e7f193c546fc6e72a67
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-05 11:50:25 +0300

    add new schema

--- galleria/models.py
+++ galleria/models.py
@@ -2,12 +2,15 @@ from django.db import models
 from django.utils import timezone
 from cloudinary.models import CloudinaryField
 
+class Cat (models.Model):
+    name = models.CharField(max_length=30)
 
 class Images(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
     date_posted = models.DateTimeField(default = timezone.now)
     image = CloudinaryField('image', default = '')
+    categs = models.ForeignKey(Cat,null=True, blank=True,on_delete=models.CASCADE)
 
     def save_image(self):
         self.save()
