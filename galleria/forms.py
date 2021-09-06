ca726126126d90c66f994523719077290d6ae279
Awadh-Awadh <awadh.awadhawadh.awadh@student.moringaschool.com>
2021-09-06 19:50:12 +0300

    add form documentation

--- galleria/forms.py
+++ galleria/forms.py
@@ -1,6 +1,10 @@
 from django import forms
 from .models import Images
-
+'''
+form creation that will will inherit from forms.Models
+include an inner class meta that defines which model to reference 
+and generate field based on model
+'''
 
 class PictureForm(forms.ModelForm):
   class Meta:
